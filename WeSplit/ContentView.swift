a24482ba904f98d623c172a01d2e80b69e2d80f1
Bornak <bornakpaul@gmail.com>
2022-01-06 01:58:05 +0530

    done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,26 +9,50 @@ import SwiftUI
 // we need this to import all the framework by swift
 
 struct ContentView: View {
-    @State private var chechAmount = ""
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 2
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 0
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+    
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form{
-            Section{
-                TextField("Amount", text: $chechAmount)
-                    .keyboardType(.decimalPad)
-            }
-            Picker("Number of people", selection: $numberOfPeople){
-                ForEach(2 ..< 100){
-                    Text("\($0) people")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2 ..< 100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section (header: Text("How much tip do you want to leave ?")){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0 ..< tipPercentages.count){
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section{
+                    Text("₹ \(totalPerPerson, specifier: "%.2f")")
                 }
             }
-            Section{
-                Text("₹ \(chechAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
